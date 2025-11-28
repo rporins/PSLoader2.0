539f0fe5260a86f481ce584975b76b98e3aa298a
rporins <rporins@users.noreply.github.com>
2025-11-28 18:12:10 +0000

    switch to servers

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@
 // Switch between test and production servers by commenting/uncommenting the URLs below
 
 // Test/Development Server
-export const API_BASE_URL = 'http://127.0.0.1:8000';
+// export const API_BASE_URL = 'http://127.0.0.1:8000';
 
 // Production Server
-// export const API_BASE_URL = 'https://fastapi-fafyfgcmaqgsbncg.uksouth-01.azurewebsites.net';
+export const API_BASE_URL = 'https://fastapi-fafyfgcmaqgsbncg.uksouth-01.azurewebsites.net';
