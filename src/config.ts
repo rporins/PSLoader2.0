9fb4eb996dd259f3cbe9176602ddddc9eac29ea2
rporins <rporins@users.noreply.github.com>
2025-11-29 01:41:27 +0000

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 // API Configuration
 // Switch between test and production servers by commenting/uncommenting the URLs below
-// Updated testing 5
+// Updated testing 6
 
 // Test/Development Server
 // export const API_BASE_URL = 'http://127.0.0.1:8000';
